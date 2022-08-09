2e8fbe21ea943b1d739d35605339123ac84c43bc
Konsta Nenonen <konstanenonen@protonmail.com>
2022-08-09 21:33:02 +0300

    Started to add firebase to the project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import Canvas from '../Canvas/Canvas';
 import styles from './App.module.scss';
+
+firebase.initializeApp({
+  // FILL THIS
+});
 
 function App() {
   return (
