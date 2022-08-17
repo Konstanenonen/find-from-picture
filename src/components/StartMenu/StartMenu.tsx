102ded3bd8f1091d2e9c5f3072d72c4592165f91
Konsta Nenonen <konstanenonen@protonmail.com>
2022-08-17 23:29:14 +0300

    Improved StartMenu styling

--- src/components/StartMenu/StartMenu.tsx
+++ src/components/StartMenu/StartMenu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import waldo from '../../images/waldo-character.png';
 import odlaw from '../../images/odlaw.jpg';
 import wizard from '../../images/wizard.png';
+import styles from './StartMenu.module.scss';
 
 interface StartMenuProps {
   start: () => void;
@@ -9,21 +10,21 @@ interface StartMenuProps {
 
 function StartMenu({ start }: StartMenuProps) {
   return (
-    <div>
-      <h1>Find From Picture</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Find From Picture</h1>
       <p>Find these characters as fast as possible from the picture</p>
-      <div>
-        <div>
-          <img src={waldo} alt="Happy Waldo" />
-          <p>Waldo</p>
+      <div className={styles.characters}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={waldo} alt="Happy Waldo" />
+          <p className={styles.characterText}>Waldo</p>
         </div>
-        <div>
-          <img src={odlaw} alt="Happy Odlaw" />
-          <p>Odlaw</p>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={odlaw} alt="Happy Odlaw" />
+          <p className={styles.characterText}>Odlaw</p>
         </div>
-        <div>
-          <img src={wizard} alt="Happy Wizard" />
-          <p>Wizard</p>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={wizard} alt="Happy Wizard" />
+          <p className={styles.characterText}>Wizard</p>
         </div>
       </div>
       <button onClick={start} type="button">
