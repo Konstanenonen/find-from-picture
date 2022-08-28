cc9caf7fa1faa839caeef5fa7047746aadb8c982
Konsta Nenonen <konstanenonen@protonmail.com>
2022-08-28 13:35:56 +0300

    Added better styles to WrongSelectionPopup so it now is an actual popup on top of the canvas

--- src/components/WrongSelectionPopup/WrongSelectionPopup.tsx
+++ src/components/WrongSelectionPopup/WrongSelectionPopup.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './WrongSelectionPopup.module.scss';
 
 function WrongSelectionPopup() {
   return (
-    <div>
+    <div className={styles.container}>
       <p>Wrong location, try again!</p>
     </div>
   );
