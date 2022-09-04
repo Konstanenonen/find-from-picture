edaf291c354ada40d41070aaa5de9fe0685479ce
Konsta Nenonen <konstanenonen@protonmail.com>
2022-09-04 10:48:57 +0300

    Added some styling to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 function Footer() {
   return (
-    <div>
-      <p>This is Footer</p>
+    <div className={styles.container}>
+      <p>Made by Konsta Nenonen</p>
+      <a href="https://github.com/Konstanenonen/find-from-picture">
+        Link to repo
+      </a>
     </div>
   );
 }
